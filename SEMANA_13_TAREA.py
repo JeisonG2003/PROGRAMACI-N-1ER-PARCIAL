ee04d5d0347394cdf69305ebf925e354a30b3181
Jeison Garcia <jt.garciaa@uea.edu.ec>
2025-03-14 07:20:45 -0500

    Update SEMANA_13_TAREA.py

--- SEMANA_13_TAREA.py
+++ SEMANA_13_TAREA.py
@@ -28,114 +28,114 @@ nombres_ciudades = ["Quito", "Guayaquil", "Ambato"]
 temperaturas = [
     [  # Ciudad 1: Quito
         [  # Semana 1
-            {"day": "Lunes", "temp": 23},
+            {"day": "Lunes", "temp": 30},
             {"day": "Martes", "temp": 27},
-            {"day": "Miércoles", "temp": 28},
-            {"day": "Jueves", "temp": 27},
-            {"day": "Viernes", "temp": 25},
+            {"day": "Miércoles", "temp": 25},
+            {"day": "Jueves", "temp": 29},
+            {"day": "Viernes", "temp": 24},
             {"day": "Sábado", "temp": 21},
-            {"day": "Domingo", "temp": 24}
+            {"day": "Domingo", "temp": 26}
         ],
         [  # Semana 2
-            {"day": "Lunes", "temp": 26},
-            {"day": "Martes", "temp": 32},
-            {"day": "Miércoles", "temp": 29},
-            {"day": "Jueves", "temp": 31},
-            {"day": "Viernes", "temp": 27},
+            {"day": "Lunes", "temp": 24},
+            {"day": "Martes", "temp": 31},
+            {"day": "Miércoles", "temp": 28},
+            {"day": "Jueves", "temp": 32},
+            {"day": "Viernes", "temp": 28},
             {"day": "Sábado", "temp": 29},
-            {"day": "Domingo", "temp": 23}
+            {"day": "Domingo", "temp": 22}
         ],
         [  # Semana 3
-            {"day": "Lunes", "temp": 26},
+            {"day": "Lunes", "temp": 29},
             {"day": "Martes", "temp": 26},
-            {"day": "Miércoles", "temp": 31},
-            {"day": "Jueves", "temp": 33},
-            {"day": "Viernes", "temp": 25},
-            {"day": "Sábado", "temp": 20},
-            {"day": "Domingo", "temp": 23}
+            {"day": "Miércoles", "temp": 30},
+            {"day": "Jueves", "temp": 31},
+            {"day": "Viernes", "temp": 22},
+            {"day": "Sábado", "temp": 30},
+            {"day": "Domingo", "temp": 32}
         ],
         [  # Semana 4
             {"day": "Lunes", "temp": 26},
-            {"day": "Martes", "temp": 22},
-            {"day": "Miércoles", "temp": 26},
-            {"day": "Jueves", "temp": 27},
-            {"day": "Viernes", "temp": 29},
-            {"day": "Sábado", "temp": 28},
+            {"day": "Martes", "temp": 21},
+            {"day": "Miércoles", "temp": 20},
+            {"day": "Jueves", "temp": 29},
+            {"day": "Viernes", "temp": 21},
+            {"day": "Sábado", "temp": 23},
             {"day": "Domingo", "temp": 31}
         ]
     ],
     [  # Ciudad 2: Guayaquil
         [  # Semana 1
-            {"day": "Lunes", "temp": 19},
-            {"day": "Martes", "temp": 22},
+            {"day": "Lunes", "temp": 29},
+            {"day": "Martes", "temp": 28},
             {"day": "Miércoles", "temp": 24},
-            {"day": "Jueves", "temp": 25},
+            {"day": "Jueves", "temp": 23},
             {"day": "Viernes", "temp": 23},
-            {"day": "Sábado", "temp": 29},
-            {"day": "Domingo", "temp": 28}
+            {"day": "Sábado", "temp": 20},
+            {"day": "Domingo", "temp": 22}
         ],
         [  # Semana 2
-            {"day": "Lunes", "temp": 29},
-            {"day": "Martes", "temp": 30},
+            {"day": "Lunes", "temp": 22},
+            {"day": "Martes", "temp": 33},
             {"day": "Miércoles", "temp": 30},
-            {"day": "Jueves", "temp": 32},
-            {"day": "Viernes", "temp": 35},
+            {"day": "Jueves", "temp": 27},
+            {"day": "Viernes", "temp": 28},
             {"day": "Sábado", "temp": 29},
             {"day": "Domingo", "temp": 33}
         ],
         [  # Semana 3
-            {"day": "Lunes", "temp": 24},
-            {"day": "Martes", "temp": 25},
-            {"day": "Miércoles", "temp": 21},
-            {"day": "Jueves", "temp": 28},
-            {"day": "Viernes", "temp": 27},
+            {"day": "Lunes", "temp": 22},
+            {"day": "Martes", "temp": 28},
+            {"day": "Miércoles", "temp": 28},
+            {"day": "Jueves", "temp": 23},
+            {"day": "Viernes", "temp": 29},
             {"day": "Sábado", "temp": 29},
-            {"day": "Domingo", "temp": 30}
+            {"day": "Domingo", "temp": 32}
         ],
         [  # Semana 4
-            {"day": "Lunes", "temp": 19},
-            {"day": "Martes", "temp": 18},
-            {"day": "Miércoles", "temp": 26},
-            {"day": "Jueves", "temp": 29},
-            {"day": "Viernes", "temp": 28},
+            {"day": "Lunes", "temp": 29},
+            {"day": "Martes", "temp": 28},
+            {"day": "Miércoles", "temp": 20},
+            {"day": "Jueves", "temp": 23},
+            {"day": "Viernes", "temp": 25},
             {"day": "Sábado", "temp": 31},
-            {"day": "Domingo", "temp": 27}
+            {"day": "Domingo", "temp": 29}
         ]
     ],
     [  # Ciudad 3: Ambato
         [  # Semana 1
-            {"day": "Lunes", "temp": 29},
-            {"day": "Martes", "temp": 19},
+            {"day": "Lunes", "temp": 26},
+            {"day": "Martes", "temp": 29},
             {"day": "Miércoles", "temp": 27},
-            {"day": "Jueves", "temp": 21},
-            {"day": "Viernes", "temp": 26},
-            {"day": "Sábado", "temp": 24},
-            {"day": "Domingo", "temp": 23}
+            {"day": "Jueves", "temp": 25},
+            {"day": "Viernes", "temp": 29},
+            {"day": "Sábado", "temp": 30},
+            {"day": "Domingo", "temp": 32}
         ],
         [  # Semana 2
             {"day": "Lunes", "temp": 24},
-            {"day": "Martes", "temp": 20},
+            {"day": "Martes", "temp": 27},
             {"day": "Miércoles", "temp": 26},
-            {"day": "Jueves", "temp": 19},
+            {"day": "Jueves", "temp": 29},
             {"day": "Viernes", "temp": 27},
             {"day": "Sábado", "temp": 22},
             {"day": "Domingo", "temp": 24}
         ],
         [  # Semana 3
-            {"day": "Lunes", "temp": 26},
+            {"day": "Lunes", "temp": 20},
             {"day": "Martes", "temp": 24},
             {"day": "Miércoles", "temp": 27},
             {"day": "Jueves", "temp": 23},
-            {"day": "Viernes", "temp": 25},
-            {"day": "Sábado", "temp": 28},
+            {"day": "Viernes", "temp": 30},
+            {"day": "Sábado", "temp": 32},
             {"day": "Domingo", "temp": 30}
         ],
         [  # Semana 4
-            {"day": "Lunes", "temp": 28},
-            {"day": "Martes", "temp": 30},
-            {"day": "Miércoles", "temp": 21},
-            {"day": "Jueves", "temp": 29},
-            {"day": "Viernes", "temp": 28},
+            {"day": "Lunes", "temp": 23},
+            {"day": "Martes", "temp": 29},
+            {"day": "Miércoles", "temp": 25},
+            {"day": "Jueves", "temp": 27},
+            {"day": "Viernes", "temp": 30},
             {"day": "Sábado", "temp": 31},
             {"day": "Domingo", "temp": 24}
         ]
